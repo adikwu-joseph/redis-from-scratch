aaee7433543c497e280f073dfd1329fd778d2073
stoppa5107 <Joegbo247@gmail.com>
2026-04-28 01:11:45 +0100

    add idle connection timeout with doubly-linked list timer

--- src/zset.h
+++ src/zset.h
@@ -1,23 +1,25 @@
 #pragma once
+
 #include "avl.h"
 #include "hashtable.h"
 
-struct ZNode {
-    AVLNode tree;
-    HNode hmap;
-    double score = 0;
-    size_t len = 0;
-    char name[0];
-};
 
 struct ZSet {
-    AVLNode *root = NULL;
-    HMap hmap;
+    AVLNode *root = NULL;   // index by (score, name)
+    HMap hmap;              // index by name
 };
 
-ZNode *zset_lookup(ZSet *zset, const char *name, size_t len);
+struct ZNode {
+    AVLNode tree;
+    HNode   hmap;
+    double  score = 0;
+    size_t  len = 0;
+    char    name[0];        // flexible array
+};
+
 bool   zset_insert(ZSet *zset, const char *name, size_t len, double score);
+ZNode *zset_lookup(ZSet *zset, const char *name, size_t len);
 void   zset_delete(ZSet *zset, ZNode *node);
 ZNode *zset_seekge(ZSet *zset, double score, const char *name, size_t len);
-ZNode *znode_offset(ZNode *node, int64_t offset);
 void   zset_clear(ZSet *zset);
+ZNode *znode_offset(ZNode *node, int64_t offset);
