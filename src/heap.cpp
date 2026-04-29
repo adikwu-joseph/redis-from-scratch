c24854322352679fefc6260b8f31e4d2311f6a91
stoppa5107 <Joegbo247@gmail.com>
2026-04-29 22:36:21 +0100

    add thread pool for async background tasks

--- src/heap.cpp
+++ src/heap.cpp
@@ -5,6 +5,13 @@ static size_t heap_parent(size_t i) {
     return (i + 1) / 2 - 1;
 }
 
+static size_t heap_left(size_t i) {
+    return i * 2 + 1;
+}
+
+static size_t heap_right(size_t i) {
+    return i * 2 + 2;
+}
 
 static void heap_up(HeapItem *a, size_t pos) {
     HeapItem t = a[pos];
--- src/heap.h
+++ src/heap.h
@@ -8,6 +8,5 @@ struct HeapItem {
     uint64_t val = 0;
     size_t *ref = NULL;
 };
-inline size_t heap_left(size_t i) { return 2 * i + 1; }
-inline size_t heap_right(size_t i) { return 2 * i + 2; }
+
 void heap_update(HeapItem *a, size_t pos, size_t len);
